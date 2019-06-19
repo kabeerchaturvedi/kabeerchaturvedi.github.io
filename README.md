501f08a06b92272cb9230682f2fa717e5533b59e
unknown <kbrchaturvedi@gmail.com>
2019-06-19 23:58:11 +0530

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# kabeerchaturvedi.github.io
---
+++ precache-manifest.38f0f9e7584f909e887936c5879ee1c3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7abe8ca787ae3c02e0409babf0c3ace4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea6c0281b2ad8747e43c",
+    "url": "/static/css/main.81f4eb41.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "ea6c0281b2ad8747e43c",
+    "url": "/static/js/main.b65bd1ab.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e6c6fe79b5b894687ad89aa1a6afe01c",
+    "url": "/static/media/A.e6c6fe79.jpg"
+  },
+  {
+    "revision": "b7ae8662404ea171a45d7f4120f398c1",
+    "url": "/static/media/College.b7ae8662.png"
+  },
+  {
+    "revision": "c7bfe8dfba84f7a33ed30db6a1f518b9",
+    "url": "/static/media/EY-logo.c7bfe8df.png"
+  },
+  {
+    "revision": "d8ccd0753ef199196454a42a0ea8e2ce",
+    "url": "/static/media/Normal.d8ccd075.png"
+  },
+  {
+    "revision": "350bc3322a60ea4dcc8bbe913b5b725e",
+    "url": "/static/media/Project.350bc332.png"
+  },
+  {
+    "revision": "38113769702ff1b73bbe7fa230080936",
+    "url": "/static/media/School.38113769.png"
+  },
+  {
+    "revision": "bb6f8e29fdf6b17fb97f623b3f42da37",
+    "url": "/static/media/cg-logo.bb6f8e29.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
